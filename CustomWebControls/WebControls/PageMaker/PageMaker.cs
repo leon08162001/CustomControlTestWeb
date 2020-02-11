cd8124723cc545a1ce930f623e4c5b71d33cda61
leon.lee <leon.lee@taipeistarbank.com.tw>
2020-02-11 09:41:06 +0800

    update

--- CustomWebControls/WebControls/PageMaker/PageMaker.cs
+++ CustomWebControls/WebControls/PageMaker/PageMaker.cs
@@ -249,13 +249,8 @@ namespace APTemplate
             }
             else
             {
-                //int pageTo;
-                //if (Context.Request.Form[PageTo.UniqueID] != null && Int32.TryParse(Context.Request.Form[PageTo.UniqueID].ToString(), out pageTo)
-                //    && pageTo == PageIndex + 1)
-                //{
-                    GetPageCount();
-                    Paging();
-                //}
+                GetPageCount();
+                Paging();
             }
         }
 
